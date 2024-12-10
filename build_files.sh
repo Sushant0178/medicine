a535f7f4f15a703ae4fb7db1bdc51b82c396cd5e
sushant kokate <91862793+Sushant0178@users.noreply.github.com>
2024-12-11 00:27:01 +0530

    ok

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,5 @@
-echo "BUILD START"
-
-# Install dependencies
-python3.9 -m pip install -r requirements.txt
-
-# Collect static files, clear old static files before collecting new ones
+#!/bin/bash
+# Build script to handle collectstatic for Django
+echo "Collecting static files..."
 python manage.py collectstatic --noinput --clear
-
-echo "BUILD END"
+# 
